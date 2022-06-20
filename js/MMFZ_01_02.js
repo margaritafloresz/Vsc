3e9a26db21b564b8cd34be9c7c5ebd7e7b359845
Margarita Flores <mfloreszatarain95@gmail.com>
2022-06-19 19:30:05 -0600

    Ya esta

--- js/MMFZ_01_02.js
+++ js/MMFZ_01_02.js
@@ -1,38 +1,34 @@
 // 1. Solicitar 3 numeros (entre el 1 y 100), definir cuál es el mayor//
-// 2. Solicitar 3 numeros (entre el 1 y 100), definir cuál es el menor//
-let numeroA = 67;
-let numeroB = 89;
-let numeroC = 25;
 
 
-if ((numeroB > numeroA) && (numeroB > numeroC)){
-    console.log ("Numero B es el mayor ");
-    if ((numeroA < numeroC )){
-        console.log ( "Numero A es el menor "); 
-    }
-    else {
-    console.log ( "Numero C es el menor ");
-}
+let numeroA = 6;
+let numeroB = 0;
+let numeroC = 2;
 
+
+if ((numeroA > numeroB) && (numeroA > numeroC)){
+    console.log ("Numero A es el mayor ");
 } 
 
-else if( (numeroA > numeroB) && (numeroA > numeroC)){
-    console.log( "Numero A es el mayor  ");
-    if ((numeroB < numeroC)){
-        console.log ("Numero B es el menor");
-    }
-    else {
-        console.log ("Numero C es el menor");
-    }
+    else if( (numeroB > numeroA) && (numeroB > numeroC)){
+    console.log( "Numero B es el mayor  ");
 }
     
 else {
     console.log ("Numero C es mayor "); 
-    if ((numeroB < numeroA )){
-        console.log ("Numero B es menor");
-    }
-    else{ 
-        console.log( "Numero A es menor");
-    }
+
 }
 
+// 2. Solicitar 3 numeros (entre el 1 y 100), definir cuál es el menor//
+if ((numeroA <numeroB) && (numeroA < numeroC)){
+    console.log ("Numero A es el menor ");
+} 
+
+    else if( (numeroB < numeroA) && (numeroB < numeroC)){
+    console.log( "Numero B es el menor  ");
+}
+    
+else {
+    console.log ("Numero C es el menor "); 
+
+}
